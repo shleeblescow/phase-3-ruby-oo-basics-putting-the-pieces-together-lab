f01db6c6751eba4d5ccf4629c643454089c8a7bb
Shelby Lee <slee337@uw.edu>
2022-09-20 09:47:52 -0600

    commit tree d252201c0cba72e06d4c613a2c6b90332a18de71

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,13 @@
-# Make your shoe class here!
+class Shoe
+    attr_reader :brand
+    attr_accessor :condition, :color, :size, :material
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
